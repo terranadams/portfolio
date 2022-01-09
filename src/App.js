f005b2fd69592d3e23c3369db3ffc2dd6ae24953
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2022-01-09 11:51:20 -0700

    setting up routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg'
 import './App.css'
 import Navigation from './Navigation'
+import Resume from './Resume'
+import Pokedex from './pokedex/Pokedex'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className='App'>
-      <Navigation />
+      {/* <Navigation /> */}
+      <Routes>
+        <Route path='/' element={<Resume />} exact />
+        <Route path='/pokedex' element={<Pokedex />} exact />
+      </Routes>
     </div>
   )
 }
---
+++ src/pokedex/Pokedex.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Pokedex = () => {
+    return (
+        <div>
+            This is the Pokedex page
+        </div>
+    )
+}
+
+export default Pokedex
