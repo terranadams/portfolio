46cb1af374695ebcff34549a2003e42cf3bf58d0
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2022-01-10 17:56:30 -0700

    new comp

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import 'bootstrap/dist/css/bootstrap.min.css' // THIS IS WHAT YOU NEED FOR THE BOOTSTRAP TO WORK
-import { useState } from 'react'
-import Navigation from './Navigation'
-import Resume from './Resume'
-import Pokedex from './pokedex/Pokedex'
-import PokemonPage from './pokedex/PokemonPage'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { ListContext } from './pokedex/contexts/ListContext'
+import "bootstrap/dist/css/bootstrap.min.css"; // THIS IS WHAT YOU NEED FOR THE BOOTSTRAP TO WORK
+import { useState } from "react";
+import Navigation from "./Navigation";
+import Resume from "./Resume";
+import Pokedex from "./pokedex/Pokedex";
+import PokemonPage from "./pokedex/PokemonPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ListContext } from "./pokedex/contexts/ListContext";
 
 function App() {
-  const [pokelist, setPokelist] = useState([])
+  const [pokelist, setPokelist] = useState([]);
 
   return (
-    <div className='App'>
+    <div className="App">
       <Navigation />
 
       <ListContext.Provider value={{ pokelist, setPokelist }}>
         <Router>
           <Routes>
-            <Route path='/' element={<Resume />} exact />
-            <Route path='/pokedex' element={<Pokedex />} exact />
-            <Route path='/pokedex/:id' element={<PokemonPage />} exact />
+            <Route path="/" element={<Resume />} exact />
+            <Route path="/pokedex" element={<Pokedex />} exact />
+            <Route path="/pokedex/:id" element={<PokemonPage />} exact />
           </Routes>
         </Router>
       </ListContext.Provider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
