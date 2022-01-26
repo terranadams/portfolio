9da433343ce3054d41b80e3702b0e02e5995babd
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2022-01-25 17:02:53 -0700

    installed react-router-bootstrap, preparing to troubleshoot navbar

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,35 +1,45 @@
 import React from "react";
 import { Nav, Navbar, NavDropdown, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
 
 const Navigation = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
         <Navbar.Brand href="/">Terran Adams</Navbar.Brand>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="experience">Experience</Nav.Link>
-            {/* <Link to='/experience'>Experience</Link> */}
-            <Nav.Link href="education">Education</Nav.Link>
-            {/* <Link to='/education'>Education</Link> */}
-            <Nav.Link href="skills">Skills</Nav.Link>
-            {/* <Link to='/skills'>Skills</Link> */}
-            <NavDropdown title="React" id="basic-nav-dropdown">
-              <NavDropdown.Item href="pokedex">Pokédex</NavDropdown.Item>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <div className="justify-content-end">
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto ">
+              {/* <LinkContainer to='/experience'><Nav.Link>Experience</Nav.Link></LinkContainer>
+            <LinkContainer to='/education'><Nav.Link>Education</Nav.Link></LinkContainer>
+            <LinkContainer to='/skills'><Nav.Link>Skills</Nav.Link></LinkContainer> */}
 
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+              <Nav.Link href="/experience">Experience</Nav.Link>
+              <Nav.Link href="/education">Education</Nav.Link>
+              <Nav.Link href="/skills">Skills</Nav.Link>
 
-            <NavDropdown title="Angular" id="basic-nav-dropdown">
-              <NavDropdown.Item href="https://macwhitehead.github.io/NgGroupProject-Trivia/" target='_blank'>Trivia Extraordinaire</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
+              <NavDropdown title="React" id="basic-nav-dropdown">
+                <NavDropdown.Item href="pokedex">Pokédex</NavDropdown.Item>
+
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+
+              <NavDropdown title="Angular" id="basic-nav-dropdown">
+                <NavDropdown.Item
+                  href="https://macwhitehead.github.io/NgGroupProject-Trivia/"
+                  target="_blank"
+                >
+                  Trivia Extraordinaire
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </div>
       </Container>
     </Navbar>
   );
