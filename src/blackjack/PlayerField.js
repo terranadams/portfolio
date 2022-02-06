9aed98c8d55b802e436e0f1c476849361cecca1b
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2022-02-06 11:26:53 -0700

    adding new card to card values

--- src/blackjack/PlayerField.js
+++ src/blackjack/PlayerField.js
@@ -8,6 +8,7 @@ const PlayerField = (props) => {
 
   const [handValue, setHandValue] = useState();
   const [initialDraw, setInitialDraw] = useState(false);
+  const [holding, setHolding] = useState(false)
 
   const getHandValue = (cards) => {
     let totalValue = 0;
@@ -51,31 +52,41 @@ const PlayerField = (props) => {
       });
   };
 
-  const playerDraw1 = () => {
-    console.log('eyyyyyy I drew another card')
+  const playerDraw1 = async () => {
+    const response = await fetch(
+      `https://deckofcardsapi.com/api/deck/${props.deckId}/draw/?count=1`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(`Player ${props.player} drew one card.`, data.cards);
+        if (props.player === "1") {
+          let newHand = [...playerOneHand, data.cards[0]]
+          setPlayerOneHand(newHand);
+          getHandValue(newHand);
+        } else {
+          let newHand = [...playerTwoHand, data.cards[0]]
+          setPlayerTwoHand(newHand);
+          getHandValue(newHand);
+        }
+      });
   }
 
-  const handleHold = () => {
-    console.log('eyyyyyy Im holding my cards')
-  }
-
-
-  // make button switch to different function after initial card draws
+  
 
   return (
     <div style={{ marginLeft: "20px" }}>
       <p>Player {props.player}</p>
-      {!initialDraw && (
+      {(!initialDraw && !holding) && (
         <Button onClick={playerDraw2} style={{ marginBottom: "20px" }}>
           Draw 2 Cards
         </Button>
       )}
-      {initialDraw && (
+      {(initialDraw && !holding) && (
         <>
           <Button onClick={playerDraw1} variant="success" style={{ marginBottom: "20px" }}>
             Draw another card
           </Button>
-          <Button onClick={handleHold} variant="warning" style={{ marginBottom: "20px" }}>
+          <Button onClick={() => setHolding(true)} variant="warning" style={{ marginBottom: "20px" }}>
             Hold
           </Button>
         </>
