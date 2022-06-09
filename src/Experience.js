a958b51b71d41008fc96ec3d8259c81987b60120
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2022-06-09 15:48:46 -0600

    updated experience details for Accela

--- src/Experience.js
+++ src/Experience.js
@@ -22,15 +22,15 @@ const Experience = () => {
             </a>
             <h3>{/* <strong>Accela, Inc.</strong> */}</h3>
             <i>April 2021 - present</i>
-            <p>Technical Support Engineer</p>
+            <p>Technical Support Engineer / API Specialist</p>
             <div className="paragraphContainer">
               <p className="content">
                 I work to diagnose issues with Accela’s software products. I
                 interact with Accela’s clients to detail replication steps of
                 the problem at hand and find a resolution for it if I can’t
                 diagnose it as a software glitch. I specialized in diagnosing
-                issues with the company’s main mobile application, which
-                involved utilizing our API using Postman to decipher where
+                issues with the company’s own API, which
+                involves using Postman to decipher where
                 errors come from in the mobile app using Trace IDs provided in
                 the API call response.
               </p>
